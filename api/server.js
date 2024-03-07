2cdbac948a4084953442ea7f9c7efab9776ad67f
Michaiah Bos <michaiahbos@yahoo.com>
2024-03-06 23:55:02 -0600

    middleware

--- api/server.js
+++ api/server.js
@@ -1,8 +1,28 @@
 const express = require('express'); // importing a CommonJS module
 
+const morgan = require('morgan') //function that takes a little bit of configuration
+
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
+
+function customMorgan(req, res, next) {
+  console.log(`you made a ${req.method} request`);
+  next();
+}
+function shortCircuit(req, res, next){
+  res.json('the request was short circuited')
+}
+function addFriends(req, res, next) {
+  req.friend = 'caleb';
+  next()
+}
+
+server.use(morgan('dev'));
+server.use(customMorgan);
+// server.use(shortCircuit);
+server.use(addFriends);
+
 
 server.use(express.json());
 
@@ -10,7 +30,7 @@ server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res) => {
   res.send(`
-    <h2>Hubs API</h2>
+    <h2>Hubs API ${req.friend}</h2>
     <p>Welcome to the Hubs API</p>
   `);
 });
